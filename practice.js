fab449e93e21ef681b0be43762be4fcbcf4a4fa5
Daniel <danielrbullington@gmail.com>
2018-11-27 16:35:46 -0600

    javascript-2-afternoon-1

--- practice.js
+++ practice.js
@@ -14,7 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+const me = {
+  name: "Daniel",
+  age: 34
+}
+//alert(me.age);
+
 
 
 
@@ -25,7 +30,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+const favoriteThings = {
+  band: "Metz",
+  food: "pizza",
+  person: "my father",
+  book: "Breakfast of Champions",
+  movie: "8 1/2",
+  holiday: "Christmas"
+}
 
 
 
@@ -33,7 +45,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Ford",
+favoriteThings.brand = "Mustang";
 
 
 
@@ -41,7 +54,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets",
+favoriteThings.book = "Harry Potter";
 
 
 
@@ -55,17 +69,15 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
-
-
+var backPack = {};
+var item = "firstPocket";
+backPack[item] = "chapstick";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
-
-
+// alert[backPack]
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -73,9 +85,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
-
-
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -96,12 +106,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
-
-
-
-
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
